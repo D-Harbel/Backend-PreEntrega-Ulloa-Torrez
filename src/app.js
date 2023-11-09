20eb4d363e7511bc526a622e1835cb3c4204f6b0
Ivan Ulloa <dgmnicolas@gmail.com>
2023-11-08 23:16:49 -0300

    preentrega

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ProductRouter = require('./routers/productRouter.js');
+const CartRouter = require('./routers/cartRouter.js');
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color:green">servidor de productos en linea</h1>')
+})
+
+app.use('/api/products', ProductRouter);
+app.use('/api/carts', CartRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor en linea, puerto ${port}`)
+})
---
+++ src/classes/CartManager.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+
+class CartManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    getCarts() {
+        if (fs.existsSync(this.path)) {
+            return JSON.parse(fs.readFileSync(this.path, "utf-8"));
+        } else {
+            return [];
+        }
+    }
+
+    createCart() {
+        let carts = this.getCarts();
+
+        let id = Date.now().toString();
+
+        let cart = {
+            id,
+            products: []
+        };
+
+        carts.push(cart);
+
+        fs.writeFileSync(this.path, JSON.stringify(carts, null, 5));
+        return cart;
+    }
+
+    addProductCart(cartId, productId, quantity) {
+        let carts = this.getCarts();
+
+        let index = carts.findIndex(cart => cart.id === cartId.toString());
+
+        if (index === -1) {
+            console.log(`No se ha encontrado ${cartId}`);
+            return;
+        }
+
+        let product = {
+            product: productId,
+            quantity: quantity
+        };
+
+        let cart = carts[index];
+        cart.products.push(product);
+
+        fs.writeFileSync(this.path, JSON.stringify(carts, null, 5));
+    }
+
+    getCartById(id) {
+        let carts = this.getCarts();
+    
+        let index = carts.findIndex(cart => cart.id.toString() === id.toString());
+    
+        if (index === -1) {
+            console.log(`Not found ${id}`);
+            return;
+        }
+    
+        return carts[index];
+    }
+    
+}
+
+const cm = new CartManager("./JSON/carrito.json");
+
+module.exports = CartManager;
